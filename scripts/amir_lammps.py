9dfb1df891c87463c033368bb1c2f6a89f4a63f0
GrzegorzKaszuba <grzegorz97kaszuba@gmail.com>
2023-09-15 13:47:56 +0200

    localsearch without checkpoints fix

--- scripts/amir_lammps.py
+++ scripts/amir_lammps.py
@@ -211,9 +211,9 @@ def lmp_energy_calculator(source_dir, target_dir, lammps_cfg, silent=False, pot_
 
         # run the simulation and get the energy
         if silent:
-            subprocess.call(f'{lammps_command} -in {os.path.join(lmp_task_dir, f"in.{name}_min")}', stdout=open(os.devnull, 'wb'))
+            subprocess.call(f'{lammps_command} -in {os.path.join(lmp_task_dir, f"in.{name}_min")}'.split(' '), stdout=open(os.devnull, 'wb'))
         else:
-            os.system(f'{lammps_command} -in {os.path.join(lmp_task_dir, f"in.{name}_min")}')
+            subprocess.call(f'{lammps_command} -in {os.path.join(lmp_task_dir, f"in.{name}_min")}'.split(' '))
         initial_energy, final_energy = extract_initial_final_energy('log.lammps')
         initial_energies[name.split('.')[0]] = initial_energy
         final_energies[name.split('.')[0]] = final_energy
@@ -301,9 +301,9 @@ def lmp_elastic_calculator(source_dir, lammps_cfg, silent=False, pot_type='meam'
         assert 'PREDICTIONTASKHERE' in os.listdir()
         # run the simulation
         if silent:
-            subprocess.call(f"{lammps_command} -in {os.path.join(os.getcwd(), 'in.elastic')}", stdout=open(os.devnull, 'wb'))
+            subprocess.call(f"{lammps_command} -in {os.path.join(os.getcwd(), 'in.elastic')}".split(' '), stdout=open(os.devnull, 'wb'))
         else:
-            os.system(f"{lammps_command} -in {os.path.join(os.getcwd(), 'in.elastic')}")
+            subprocess.call(f"{lammps_command} -in {os.path.join(os.getcwd(), 'in.elastic')}".split(' '))
 
         # extract elastic_vector from the log file
         elastic_vector = extract_elastic_vector("log.lammps")
